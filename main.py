ae7a7d25c90a597476a7bd7f314b98b69222d8a4
Taylor Hiedeman <thiede2@wgu.edu>
2026-08-27 21:50:20 -0700

    MLR Model Initial Completion  with Visualizations

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import pandas as pd
-import statsmodels.api as sm
-import matplotlib.pyplot as plt
+
 
 url = 'https://data.montgomerycountymd.gov/resource/v76h-r7br.csv?$limit=350000&$offset=0'
 
-df = pd.read_csv(url)
-
-print(df.head())
-
-###Testing commit 2
 
